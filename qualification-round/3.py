4be6ab6d487f12e2222a91d299097ba2ab889199
alexanderjaramillo <alexunjm@gmail.com>
2020-04-04 01:13:23 -0500

    part 3 solved

--- qualification-round/3.py
+++ qualification-round/3.py
@@ -1,35 +1,38 @@
+from math import gcd
+
 t = int(input())
-letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-
-def isPrime(n):
-    middle = 0
-    if n % 2 == 0:
-        middle = n // 2
-    else:
-        middle = (n - 1) // 2
-    for i in range(middle, 1, -1):
-        if n % i == 0:
-            return False
-    # print("primo: {}".format(n))
-    return True
-
-arr = []
-def primeArr(n):
-
-    start = 3
-    arrLen = len(arr)
-    if arrLen > 0:
-        start = arr[arrLen - 1] + 1
-    for num in range(start, n + 1):
-        if isPrime(num):
-            arr.append(num)
-    return arr
 
 def solve():
     n, l = [int(s) for s in input().split(" ")] # read a list of integers, 2 in this case
-    pangram = [int(s) for s in input().split(" ")] # read a list of integers, 2 in this case
-    # print(len(pangram))
-    return primeArr(n)
+    arr = input() # read a list of integers, 2 in this case
+    crypto = [int(s) for s in arr.split(" ")] # read a list of integers, 2 in this case
+    primes = [0] * (l+1)
+    for i in range(l):
+        if crypto[i] != crypto[i+1]:
+            gcdCalc = gcd(crypto[i], crypto[i+1])
+            primes[i+1] = gcdCalc
             
-for i in range(1, t + 1):
+            for j in range(i, -1, -1):
+                # print("calculating: {} / {}".format(crypto[j], primes[j+1]))
+                primes[j] = crypto[j] // primes[j+1]
+
+            # print("crypto: {}".format(crypto))
+            for j in range(i+2, l+1):
+                # print("primes: {}".format(primes))
+                # print("calculating: {} / {}".format(crypto[j-1], primes[j-1]))
+                primes[j] = crypto[j-1] // primes[j-1]
+
+            break
+    print(primes)
+    setOfPrimes = []
+    for p in primes:
+        if p not in setOfPrimes:
+            setOfPrimes.append(p)
+    
+    setOfPrimes = sorted(setOfPrimes)
+    print('setOfPrimes: {}'.format(setOfPrimes))
+    res = ''.join([chr(ord('A')+setOfPrimes.index(prime)) for prime in primes])
+    return res
+            
+for i in range(1, t+1):
     print("Case #{}: {}".format(i, solve()))
